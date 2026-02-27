a82d07e7461289646f6b2cd4bfbed16b696e8f27
zhujun-123 <zj13208191651@gmail.com>
2026-02-27 21:14:05 +0800

    chore: 更新GitHub链接为自己的仓库

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -71,19 +71,19 @@ export function Menu() {
               </li>
               <li className="flex gap-2 items-center">
                 <a
-                  href="https://github.com/ourongxing/newsnow"
+                  href="https://github.com/zhujun-123/hotspot-web"
                 >
                   <img
                     alt="GitHub stars badge"
-                    src="https://img.shields.io/github/stars/ourongxing/newsnow?logo=github&style=flat&labelColor=%235e3c40&color=%23614447"
+                    src="https://img.shields.io/github/stars/zhujun-123/hotspot-web?logo=github&style=flat&labelColor=%235e3c40&color=%23614447"
                   />
                 </a>
                 <a
-                  href="https://github.com/ourongxing/newsnow/fork"
+                  href="https://github.com/zhujun-123/hotspot-web/fork"
                 >
                   <img
                     alt="GitHub forks badge"
-                    src="https://img.shields.io/github/forks/ourongxing/newsnow?logo=github&style=flat&labelColor=%235e3c40&color=%23614447"
+                    src="https://img.shields.io/github/forks/zhujun-123/hotspot-web?logo=github&style=flat&labelColor=%235e3c40&color=%23614447"
                   />
                 </a>
               </li>
